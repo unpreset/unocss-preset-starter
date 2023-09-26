3580cd1f8eee61ce063859de1578f52bf5354b0e
Chizuki <chizukicn@outlook.com>
2023-09-26 14:19:37 +0800

    chore: upgrade deps (#2)
    * chore: update playground
    
    * chore: update
    
    * chore: upgrade deps
    
    * chore: lint fix
    
    ---------
    
    Co-authored-by: Chris <hizyyv@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
-import { type Preset, definePreset } from 'unocss'
+import { definePreset } from '@unocss/core'
 
 export interface StarterOptions {
-
+  span?: number
 }
 
-export function presetStarter(_options: StarterOptions = {}): Preset {
-  return definePreset({
+export const presetStarter = definePreset((_options: StarterOptions = {}) => {
+  const span = _options.span || 12
+
+  return {
     name: 'unocss-preset-starter',
+    // Customize your preset here
     rules: [
       ['custom-rule', { color: 'red' }],
+      [
+        /col-(\d+)/,
+        ([_, s]) => ({ width: `calc(${s} / ${span} * 100%)` }),
+        { autocomplete: 'col-<span>' },
+      ],
     ],
-    // Customize your preset here
-  })
-}
+    // Customize AutoComplete
+    autocomplete: {
+      shorthands: {
+        span: Array.from({ length: span }, (_, i) => `${i + 1}`),
+      },
+    },
+  }
+})
