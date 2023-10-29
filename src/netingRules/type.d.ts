91a52c538732c04c14530746fd1ceda6cb70e472
jonathan <bcbg92@hotmail.com>
2023-10-30 00:40:14 +0100

    flexbox update

--- src/netingRules/type.d.ts
+++ src/netingRules/type.d.ts
@@ -92,4 +92,3 @@ type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] exte
   : Enumerate<N, [...Acc, Acc['length']]>
 
 type IntRange<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>
-
