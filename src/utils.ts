2fedb7d2a41958571ba0d2c351fa08f9187d8397
jonathan <bcbg92@hotmail.com>
2023-12-05 15:32:40 +0100

    removeDuplicateArrayPaddingOrMargin function

---
+++ src/utils.ts
@@ -0,0 +1,40 @@
+/**
+ * Description
+ REMOVE DUPLICATES IN MARGIN OR PADDING TO AVOID REPETITION IN CSS OUTPUT
+ @example
+  margin : 2px 2px 2px 2px
+ */
+export const removeDuplicateArrayPaddingOrMargin = (array: Array<string>) => {
+	switch (array.length) {
+		case 2:
+			if (array[0] === array[1]) {
+				array.pop();
+			}
+			if ([...new Set(array)].length === 1) {
+				array.splice(1, 2);
+			}
+			break;
+		case 3:
+			if (array[1] === array[2]) {
+				array.pop();
+			}
+			if ([...new Set(array)].length === 1) {
+				array.splice(1, 3);
+			}
+			break;
+		case 4:
+			if (array[0] === array[2]) {
+				if (array[1] === array[3]) {
+					array.splice(3, 1);
+				}
+				array.splice(2, 1);
+			}
+			if ([...new Set(array)].length === 1) {
+				array.splice(1, 3);
+			}
+			break;
+		default:
+			throw new Error("ARRAY IS TOO SHORT MUST BE AN ERROR");
+	}
+	return array;
+};
