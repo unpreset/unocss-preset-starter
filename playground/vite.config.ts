9c8c7ed307f50c51fe80f91ae9d06f587527c4ad
Chris <hizyyv@gmail.com>
2024-11-07 15:20:27 +0800

    chore: update

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,6 +1,6 @@
+import Vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
